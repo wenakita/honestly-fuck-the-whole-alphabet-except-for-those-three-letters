c618c8eae0e41adecd0483ae985c7e3c4e508ec6
exoticsolutions <rafajr1263@gmail.com>
2024-05-16 18:55:38 -0400

    o

--- my-project/src/componets/Balances.jsx
+++ my-project/src/componets/Balances.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { findId } from "../requests/friendCalls";
-import { useWallets } from "@privy-io/react-auth";
+import { useWallets, usePrivy } from "@privy-io/react-auth";
 import { readContract } from "@wagmi/core";
 import { config } from "../config";
 import FriendTechABi from "../abi/FriendTechABi";
 import { uintFormat } from "../formatters/format";
 import { Link } from "react-router-dom";
-
+import { useBalance } from "wagmi";
 function Balances() {
+  const { exportWallet } = usePrivy();
   const [userHoldings, setUserHoldings] = useState([]);
   const [holdingsDataFound, setHoldingsDataFound] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
@@ -15,6 +16,10 @@ function Balances() {
   const { wallets } = useWallets();
   const w0 = wallets[0];
   const userAddress = w0?.address;
+  const ETHBalance = useBalance({
+    address: userAddress,
+  });
+
   useEffect(() => {
     getShareHoldings();
   }, []);
@@ -101,9 +106,22 @@ function Balances() {
           border="0"
         />
       </div>
+      <div className="flex justify-center">
+        <button
+          className="text-white border border-slate-500 p-2 rounded-xl hover:bg-white hover:text-black"
+          onClick={exportWallet}
+        >
+          Export Wallet
+        </button>
+      </div>
       <div className="mt-10">
         <h3 className="text-white flex justify-center font-mono font-bold text-[10px]">
           Your unique deposit address: {w0?.address}
+        </h3>
+      </div>
+      <div className="text-center">
+        <h3 className="text-white text-[10px] font-mono mt-2">
+          ETH Balance: {ETHBalance?.data?.formatted}
         </h3>
       </div>
       <h3 className="text-white text-center mt-10">Your balances: </h3>
