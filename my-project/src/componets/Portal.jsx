7cdbd47103469d60033d75005a3ea587a78a057c
exoticsolutions <rafajr1263@gmail.com>
2024-05-28 22:06:07 -0400

    o

--- my-project/src/componets/Portal.jsx
+++ my-project/src/componets/Portal.jsx
@@ -37,7 +37,7 @@ function Portal() {
       </div>
 
       <div className="flex justify-center mt-10">
-        {authenticated && wallet ? (
+        {ready && authenticated && wallet ? (
           <button
             className="border border-slate-500 rounded-lg text-white p-2 text-[20px] hover:bg-white hover:text-black"
             onClick={logout}
